0fefe4575f0f074f101b593a2895de4e0455c1dc
andabener123 <113873623+andabener123@users.noreply.github.com>
2022-09-19 11:24:41 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo "NAMA             : RAYHAN MSP <br>";
+ echo "NIM              : 19211109 <br>";
+ echo "JURUSAN/KELAS    : Sistem Informasi/19.3B.03 <br>";
+ }
+}
